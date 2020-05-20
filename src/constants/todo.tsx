6d0d063d142435f5fc3d69b6526e802973a93fd1
DianaLeee <yeonylee94@gmail.com>
2020-05-21 00:24:26 +0900

    Add style on TodoItem, GNB

--- src/constants/todo.tsx
+++ src/constants/todo.tsx
@@ -7,19 +7,49 @@ export interface ITodo {
 export const todoItems: ITodo[] = [
   {
     id: 0,
-    text: "재난 지원금 신청하기",
+    text: "0: 재난 지원금 신청하기",
     done: true,
     img: "",
   },
   {
     id: 1,
-    text: "재활용품 버리기",
+    text: "1: 재활용품 버리기",
     done: false,
     img: "",
   },
   {
     id: 2,
-    text: "침실 블라인드 구매하기",
+    text: "2: 침실 블라인드 구매하기",
+    done: false,
+    img: "",
+  },
+  {
+    id: 3,
+    text: "3: 침실 블라인드 구매하기",
+    done: false,
+    img: "",
+  },
+  {
+    id: 4,
+    text: "4: 긴텍스트어떻게나오는지처리하기기이이이이이이ㅣ이잉이이이잉",
+    done: true,
+    img: "",
+  },
+  {
+    id: 5,
+    text: "5: 침실 블라인드 구매하기",
+    done: false,
+    img: "",
+  },
+  {
+    id: 6,
+    text: "6: 재활용품 버리기",
+    done: false,
+    img: "",
+  },
+  {
+    id: 7,
+    text: "7: asd",
     done: false,
     img: "",
   },
