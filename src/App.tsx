58f13b01e87d7f854f6bfdbc9e57b5cf3cb45ebe
DianaLeee <yeonylee94@gmail.com>
2020-05-20 13:42:40 +0900

    Add router & Define Sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,55 @@
 import React from "react";
+import NavTab from "./components/NavTab";
+import { All, Done, Active } from "./pages";
+import { BrowserRouter, Route } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div`
+const TopHeaderWrapper = styled.div`
+  width: 100vw;
+  height: 10vh;
+
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 0 auto;
-  height: 100vh;
+
+  position: fixed;
+  top: 0;
+  z-index: 100;
+  background: #ffffff;
+
+  font-size: 1.5rem;
+  letter-spacing: 0.1rem;
+`;
+
+const NavTabWrapper = styled.div`
+  width: 100vw;
+  height: 5vh;
+
+  position: fixed;
+  top: 10vh;
+  z-index: 100;
+  background: #ffffff;
+`;
+
+const MainContentWrapper = styled.div`
+  height: 85vh;
+  margin-top: 15vh;
+  background: #ffffff;
 `;
 
 function App() {
   return (
-    <Container>
-      Hello World
-      <span role="img" aria-label="unicorn">
-        🦄
-      </span>
-    </Container>
+    <BrowserRouter>
+      <TopHeaderWrapper>My Reminder</TopHeaderWrapper>
+      <NavTabWrapper>
+        <NavTab />
+      </NavTabWrapper>
+      <MainContentWrapper>
+        <Route exact path="/" component={All} />
+        <Route path="/active" component={Active} />
+        <Route path="/done" component={Done} />
+      </MainContentWrapper>
+    </BrowserRouter>
   );
 }
 
