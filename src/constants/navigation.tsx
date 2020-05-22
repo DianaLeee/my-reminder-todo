a981010aebf88ebd606418211e1a58969cfc8c99
DianaLeee <yeonylee94@gmail.com>
2020-05-22 20:52:02 +0900

    Declare reusable constants

--- src/constants/navigation.tsx
+++ src/constants/navigation.tsx
@@ -1,18 +1,23 @@
 export interface INavigation {
   title: string;
   path: string;
+  state?: "ALL" | "ACTIVE" | "DONE" | undefined;
 }
+
 export const navItems: INavigation[] = [
   {
     title: "All",
     path: "/",
+    state: "ALL",
   },
   {
     title: "Active",
     path: "/active",
+    state: "ACTIVE",
   },
   {
     title: "Done",
     path: "/done",
+    state: "DONE",
   },
 ];
